499b91dda260e569c4cb87de84d4dfd5fca87f27
socar-humphrey <ahnsv@bc.edu>
2021-05-12 21:38:53 +0900

    feat: demo golang server를 추가한다

---
+++ demo/golang-server/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/labstack/gommon/log"
+)
+
+func main() {
+	e := echo.New()
+	e.Logger.SetLevel(log.DEBUG)
+	e.Pre(middleware.RemoveTrailingSlash())
+	e.Use(middleware.Logger())
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization},
+		AllowMethods: []string{echo.GET, echo.HEAD, echo.PUT, echo.PATCH, echo.POST, echo.DELETE},
+	}))
+	e.GET("/v1/api/healthcheckz", func(c echo.Context) error {
+		c.Logger().Info("health check")
+		return c.String(http.StatusOK, "healthy")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
+}
